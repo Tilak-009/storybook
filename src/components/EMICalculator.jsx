23df3c4e0a70c330bc25b48e53b2094aeba735c5
Tilak Naik <jalavalli321@gmail.com>
2025-01-29 10:26:42 +0530

    story book codes

---
+++ src/components/EMICalculator.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import image1 from "./images/image1.webp"
+import"./Emicalculator.css"
+
+const EMICalculator = () => {
+  const [loanAmount, setLoanAmount] = useState(469000); 
+  const [duration, setDuration] = useState(4); 
+  const rateOfInterest = 14.5; 
+
+  const calculateEMI = () => {
+    const monthlyRate = rateOfInterest / (12 * 100);
+    const emi =
+      (loanAmount * monthlyRate * Math.pow(1 + monthlyRate, duration * 12)) /
+      (Math.pow(1 + monthlyRate, duration * 12) - 1);
+    return emi.toFixed(0);
+  };
+
+  return (
+    <div className="calculator">
+      <h2 className="heading">EMI Calculator</h2>
+      <p className="description">
+        Avail upto 100% of the car value in finance at attractive interest rates
+      </p>
+
+      <div className="stickbar">
+        <label className="joystick">
+          <h3>Loan Amount</h3>
+        </label>
+       
+        <input
+          type="range"
+          min="0"
+          max="521000"
+          value={loanAmount}
+          onChange={(e) => setLoanAmount(parseInt(e.target.value))}
+          className="w-full"
+        />
+        <div className="interest">{`₹${(loanAmount / 100000).toFixed(
+          2
+        )} Lakh`}</div>
+      </div>
+        
+        {/* <img src={image1}/> */}
+      
+      <div className="time">
+        <label className="duration">
+          <b>Duration</b> in years
+        </label>
+        <div className="boxes">
+          {[1, 2, 3, 4, 5].map((year) => (
+            <button
+              key={year}
+              className={`rounded ${
+                duration === year
+                  ? "bg-orange-500 text-white"
+                  : "bg-gray-100 text-gray-700"
+              }`}
+              onClick={() => setDuration(year)}
+            >
+              {year}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      <div className="intrests">
+        <p className="rateofintrset">
+          Rate of interest @ {rateOfInterest}% for {duration} years
+        </p>
+        <p className="monthly emi">
+          <b>Your Monthly EMI:</b>
+          <div> <h1>₹{calculateEMI()}</h1></div>
+        </p>
+      </div>
+
+      <button className="r-btn">
+        Interested In Loan
+      </button>
+      <p className="loanintrest">
+        *Interest rate and loan amount offered may vary subject to customer risk
+        profile
+      </p>
+    </div>
+  );
+};
+
+export default EMICalculator;
---
+++ src/components/emicalculator.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import EMICalculator from "./EMICalculator";
+
+export default {
+  title: "Components/EMICalculator",
+  component: EMICalculator,
+};
+
+export const Default = () => <EMICalculator />;
