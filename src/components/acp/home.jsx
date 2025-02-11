a646db150c47c00e973baa3064d786161f97f9f8
Tilak Naik <jalavalli321@gmail.com>
2025-02-11 23:51:43 +0530

    with css of acecoinpay

--- src/components/acp/home.jsx
+++ src/components/acp/home.jsx
@@ -1,5 +1,6 @@
 import { useState,useEffect } from "react";
-import {  FaCheckCircle, FaCircle, FaDotCircle, FaPen } from 'react-icons/fa';
+import {   FaCheckCircle, FaCircle, FaDotCircle, FaGooglePlay, FaPen } from 'react-icons/fa';
+import "./home.css"
 
 
 
@@ -16,15 +17,17 @@ function Homepage(){
     return(
 
 <div className="home">
+    <div className="logo">
     <div className="logo1">AceCoin</div>
     <div className="logo2">Pay</div>
-        
-    <p>{time}</p>
+    <div className="timer">{time}</div>
+
+    </div>
     <div className="cardg">
-        <strong>card number  <FaPen/>
-        </strong>
+        <div className="card-no"><strong>card number  
+        </strong></div>
    
-        <i>Edit</i>
+        <i><FaPen/>Edit</i>
         <div className="etrcrd"> enter the 16-digit card number on the card</div>
         <div style={{
             border:"1px solid #ccc",
@@ -43,6 +46,57 @@ function Homepage(){
   <FaCheckCircle color="blue"/>
 </div>
 
+
+<div className="ccv">
+    <div className="crd">
+    <strong>CVV Number</strong>
+    <p>Enter the 3 or 4  digit number on card</p></div>
+    <div className="cvv-input">
+        <input style={{
+            width:"350px",
+            height:"50px"
+        }} 
+         type="text" maxLength={4} />
+         
+         {/* <FaMore/> */}
+
+
+    </div>
+
+</div>
+
+<div className="expdate">
+    <strong>Expiry date</strong>
+    <p>enter the expiration date of the card</p>
+</div>
+<div className="month">
+<input style={{
+            width:"150px",
+            height:"50px"
+        }} 
+         type="text" maxLength={4} />          /
+</div>
+<div className="date">
+<input style={{
+            width:"150px",
+            height:"50px"
+        }} 
+         type="text" maxLength={4} />          
+
+</div>
+
+<div className="passwd">
+    <div className="pass"><strong>password</strong>
+    <p>enter the dynamic password</p></div>
+</div>
+<div className="inpass">
+<input style={{
+            width:"150px",
+            height:"50px"
+        }} 
+         type="password" maxLength={10} />  
+</div>
+<button onClick={SubmitEvent}>pay n</button>
         </div>
     </div>
 </div>
