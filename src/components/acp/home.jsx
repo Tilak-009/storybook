892de5aad6f923f6d131651f6db03e924d53ae26
Tilak Naik <jalavalli321@gmail.com>
2025-02-12 15:34:49 +0530

    updated css

--- src/components/acp/home.jsx
+++ src/components/acp/home.jsx
@@ -1,5 +1,5 @@
 import { useState,useEffect } from "react";
-import {   FaCheckCircle, FaCircle, FaDotCircle, FaGooglePlay, FaPen } from 'react-icons/fa';
+import {   FaCaretSquareLeft, FaCheckCircle, FaCircle, FaDotCircle, FaGooglePlay, FaPen, FaSquare } from 'react-icons/fa';
 import "./home.css"
 
 
@@ -15,88 +15,128 @@ function Homepage(){
     
     
     return(
+<div className="outline">
+<div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    
+    }}>
+      <div style={{
+        border: "1px solid black",
+        width: "920px",
+        height: "620px",
+        display:"flex",
+        // justifyContent:"flex-end"
+      }}><div className="home">
 
-<div className="home">
-    <div className="logo">
-    <div className="logo1">AceCoin</div>
-    <div className="logo2">Pay</div>
-    <div className="timer">{time}</div>
+      <div className="logo">
+      <div className="lg" style={{ marginLeft: "50px"}}>
 
-    </div>
-    <div className="cardg">
-        <div className="card-no"><strong>card number  
-        </strong></div>
+      <div className="logo1">AceCoin</div>
+      <div className="logo2">Pay</div></div>
+      <div className="timer">{time}</div>
+  
+      </div>
+      <div className="cardg">
+          <div className="card-no"><strong>card number  
+          </strong></div>
+     
+          <i><FaPen/>Edit</i>
+          <div className="etrcrd"> enter the 16-digit card number on the card</div>
+          <div style={{
+              border:"1px solid #ccc",
+              width:"600px",
+              height:"50px",
+          }}>
+             <div class="input-container">
+    <input type="text" class="input-part" maxlength="4" opacity={0.4} width="2px"/> <FaCircle size={12} color="#ccc" opacity={0.5} />
+  
+    <input type="text" class="input-part" maxlength="4" opacity={0.4} /> <FaCircle size={12} color="#ccc" opacity={0.5} />
+  
+    <input type="text" class="input-part" maxlength="4" opacity={0.4}/> <FaCircle size={12} color="#ccc" opacity={0.5} />
+  
+    <input type="text" class="input-part" maxlength="4" opacity={0.4}/> <FaCircle size={12} color="#ccc" opacity={0.5} />
+  
+    <FaCheckCircle color="blue"/>
+  </div>
+  
+  
+  <div className="ccv">
+      <div className="crd">
+  CVV Number
+      <p>Enter the 3 or 4  digit number on card</p></div>
+      <div className="cvv-input">
    
-        <i><FaPen/>Edit</i>
-        <div className="etrcrd"> enter the 16-digit card number on the card</div>
+      {/* <FaSquare className="crdicon"/> */}
+          <input style={{
+              width:"150px",
+              height:"50px"
+          }} 
+           type="text" maxLength={4} 
+           />
+         <FaSquare className="crdicon"/>
+         </div>
+      
+  
+  </div>
+  <div className="times">
+  <div className="expdate">
+      Expiry date
+      <p>enter the expiration date of the card</p>
+  </div>
+  <div className="month">
+  <input style={{
+              width:"100px",
+              height:"50px"
+          }} 
+           type="text" maxLength={4} />          /         
+  
+  
+  <input style={{
+              width:"100px",
+              height:"50px"
+          }} 
+           type="text" maxLength={4} />          
+  <FaSquare className="timeicon"/>
+  </div>
+  </div>
+  
+  <div className="passwd">
+      <div className="pass">password
+      <p>enter the dynamic password</p></div>
+  
+  <div className="inpass">
+  <div className="painpt"><input style={{
+              width:"150px",
+              height:"50px"
+          }} 
+           type="password" maxLength={10} />
+</div>
+           <FaSquare className="passicon"/>  
+  </div>
+  </div>
+  <div className="payn">
+  <button onClick={SubmitEvent}>pay now</button></div>
+          </div>
+      </div>
+  </div>
+  
+  
+  
         <div style={{
-            border:"1px solid #ccc",
-            width:"600px",
-            height:"50px",
+          display: "flex",
+          border: "1px solid white",
+          height: "595px",
+          width: "350px",
+          flexDirection: "column",
+          
         }}>
-           <div class="input-container">
-  <input type="text" class="input-part" maxlength="4" opacity={0.4} width="2px"/> <FaCircle size={12} color="#ccc" opacity={0.5} />
-
-  <input type="text" class="input-part" maxlength="4" opacity={0.4} /> <FaCircle size={12} color="#ccc" opacity={0.5} />
-
-  <input type="text" class="input-part" maxlength="4" opacity={0.4}/> <FaCircle size={12} color="#ccc" opacity={0.5} />
-
-  <input type="text" class="input-part" maxlength="4" opacity={0.4}/> <FaCircle size={12} color="#ccc" opacity={0.5} />
-
-  <FaCheckCircle color="blue"/>
-</div>
-
-
-<div className="ccv">
-    <div className="crds">
-    CVV Number</div>
-    <div className="inptcard">Enter the 3 or 4  digit number on card</div>
-    <div className="cvv-input">
-        <input style={{
-            width:"350px",
-            height:"50px"
-        }} 
-         type="text" maxLength={4} />
-
-    </div>
-
-</div>
-
-<div className="expdate">
-    <strong>Expiry date</strong>
-    <p>enter the expiration date of the card</p>
-</div>
-<div className="month">
-<input style={{
-            width:"150px",
-            height:"50px"
-        }} 
-         type="text" maxLength={4} />          /
-</div>
-<div className="date">
-<input style={{
-            width:"150px",
-            height:"50px"
-        }} 
-         type="text" maxLength={4} />          
-
-</div>
-
-<div className="passwd">
-    <div className="pass"><strong>password</strong>
-    <p>enter the dynamic password</p></div>
-</div>
-<div className="inpass">
-<input style={{
-            width:"150px",
-            height:"50px"
-        }} 
-         type="password" maxLength={10} />  
-</div>
-<button onClick={SubmitEvent}>pay n</button>
         </div>
+      </div>
     </div>
-</div>
+  
+    </div>
 
 
 
