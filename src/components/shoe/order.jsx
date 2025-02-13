c42230af869a4b3b70ef9aa0e152573632014fe8
Tilak Naik <jalavalli321@gmail.com>
2025-02-13 16:28:03 +0530

    with shoe order

---
+++ src/components/shoe/order.jsx
@@ -0,0 +1,22 @@
+
+function Shoeorder(){
+
+    return(
+    <div style={{
+        display:"flex",
+        border:"1px solid black",
+        flexDirection:"row",
+    }}>
+             <div style={{
+        height: "100%",
+        width: "1px",
+        backgroundColor: "black",
+        position: "absolute",
+        left: "50%"
+      }}>hhbhuh9j</div>
+    
+
+    </div>)
+    }
+    
+    export default Shoeorder;
---
+++ src/components/shoe/order.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Shoeorder from "./order";
+
+export default {
+  title: "components/shoe/order",
+  component: Shoeorder,
+};
+
+export const Default = () => <Shoeorder/>;
