3defeef6cb024b88d04abd072fc38088cd633588
Tilak Naik <jalavalli321@gmail.com>
2025-02-19 21:43:38 +0530

    updated hotel mngmt

--- src/components/hotel/hotel.jsx
+++ src/components/hotel/hotel.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { FaRegSave, FaSave } from "react-icons/fa";
 import "./hotel.css";
-import subimg from "./images/bg-small.jpg"
+import logo from "./images/logo.jpeg";
+import main from "./images/mainbg.webp"
 
 function Managment(){
 
@@ -14,6 +15,8 @@ function Managment(){
             display:"flex",
             justifyContent:"center",
             alignItems:"center",
+            backgroundImage: `url(${main})`,
+
             
 
         }}> 
@@ -24,6 +27,7 @@ function Managment(){
             width:"550px",
             height:"1100px",
             borderRadius:"50px",
+            backgroundColor:'white'
 
         }}>
             <div className="subin">
@@ -31,6 +35,8 @@ function Managment(){
             border:"1px solid black",
             width:"550px",
             height:"350px",
+            
+
             borderRadius:"50px",
             
             
@@ -46,6 +52,7 @@ function Managment(){
             // position:"absolute",
             // left:"15px",
             // top:"50px",
+            backgroundImage:`url(${logo})`
             
 
         }}>
@@ -53,10 +60,19 @@ function Managment(){
             </div>
             </div>
              
-            <h3 className="name"> john abraham</h3>
+            <p><h3 className="name"> john abraham</h3>
             <FaRegSave className="saved"/>
-            </div>
+            <br/>hotel guest</p>
 
+            
+            </div>
+            <div className="reco">recomanded for you</div>
+            <div className="content">
+            <div style={{
+                border:"1px solid black"
+            }}>
+            </div>
+            </div>
         </div>
         </div>
 
