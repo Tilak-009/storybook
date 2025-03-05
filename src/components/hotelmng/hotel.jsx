cc7f40e7be1b0fa6597edbeae20c792f0eef60ce
Tilak Naik <jalavalli321@gmail.com>
2025-03-05 23:36:08 +0530

    updated hotel mngment

--- src/components/hotelmng/hotel.jsx
+++ src/components/hotelmng/hotel.jsx
@@ -1,7 +1,7 @@
 import React from "react"; 
-import { FaFacebook ,FaEllipsisH,FaBars, FaArrowDown, FaCaretDown, FaSearch, FaCaretLeft, FaArrowRight} from 'react-icons/fa';
+import { FaFacebook ,FaEllipsisH,FaBars, FaArrowDown, FaCaretDown, FaSearch, FaCaretLeft, FaArrowRight, FaUtensils, FaHamburger, FaPizzaSlice, FaPastafarianism, FaPaste, FaUtensilSpoon, FaFish, FaDrumstickBite, FaCuttlefish, FaTimes, FaTimesCircle, FaClock, FaInfoCircle, FaHome} from 'react-icons/fa';
 import "./hotel1.css"
-import dish from "./images/dish2.jpg"
+import dish from "./images/dish.jpg"
 
 function Hotelmanagment(){
     return(
@@ -12,20 +12,22 @@ function Hotelmanagment(){
                     display:"flex",
                     flexDirection:"row",
             border:"1px solid black",
-            height:"750px",
-            width:"1300px",
+            height:"1500px",
+            width:"1080px",
             borderRadius:"30px",
-            backgroundColor:"white"
+            backgroundColor:"white",
+            
             }}>   
             
             {/* myleft */}
               <div style={{
                 border:"1px solid black",
-                height:"740px",
+                height:"1500px",
                 width:"750px",
                 borderRadius:"30px",
                 // borderLeftColor:"white",
-                borderRightColor:"white"}}>
+                borderRightColor:"white",
+                display:"flex"}}>
                 
                <div className="header">
                      <FaBars className="bari"/> <b>125  estern costume St. <FaCaretDown className="down"/></b>
@@ -36,12 +38,13 @@ function Hotelmanagment(){
                      <div className="decor">
                         <b>You Don't Need a Silver<br/>
                         fork to Eat good food</b><br/>
+                        <FaEllipsisH/>
                         <div className="lm"><i className="lemo">learn more </i><FaArrowRight className="rit" /></div>
                         <div className="imgcircle"
                              style={{
                                 border:"1px solid green",
                                 borderRadius:"100px",
-                                width:230,
+                                width:240,
                                 height:220,
                                 position:"absolute",
                                 right:120,
@@ -55,7 +58,88 @@ function Hotelmanagment(){
                             <img src={dish} />
                             </div>
                         </div>
-                     </div>
+
+                        <div className="dishc">
+                            <b>Choose from different Catogories</b><br/>
+                            <p className="cato">
+                               
+                                <b className="ball"><FaUtensils className="All" />All </b>
+                                <b className="bbg"> <FaHamburger className="burger"/> burger</b>
+                                <b className="bpz"><FaPizzaSlice className="pizza"/>pizza</b>
+                                <b className="bps"><FaUtensilSpoon className="pasta"/>pasta</b>
+                                <b className="bfs"> <FaFish className="fish"/>Fish</b> 
+                                <b className="bck"><FaDrumstickBite  className="chicken"/>chicken</b>
+                                <b className="bbf">   <FaCuttlefish className="beef"/>beef</b>
+                              </p>
+
+
+                        </div>
+
+
+
+
+
+
+                        <div className="imgbxs">
+                            <div className="imgbx1" style={{
+                                border:"1px solid black",
+                                width:150,
+                                height:250
+                            }}> <div className="img1"><img src={dish}/></div> <b>Restaurent Name<br/>
+                            <p className="imgdsc1"><FaClock/><i>15 min</i>  <FaInfoCircle className="i1"/> <FaInfoCircle className="i2"/></p></b><br/>
+                            <p className="home1"><FaHome/><i>$3.45 free delivery</i></p>
+
+                            </div>
+
+
+
+
+
+                        
+                       
+                            <div className="imgbx2" style={{
+                                border:"1px solid black",
+                                width:150,
+                                height:250
+                            }}> <div className="img2"><img src={dish}/></div> <b>Restaurent Name<br/>
+                            <p className="imgdsc2"><FaClock/><i>15 min</i>  <FaInfoCircle className="info1"/> <FaInfoCircle className="info2"/></p></b><br/>
+                            <p className="home2"><FaHome/><i>$3.45 free delivery</i></p>
+
+                            </div>  
+
+
+
+
+
+
+
+                            <div className="imgbx3" style={{
+                                border:"1px solid black",
+                                width:150,
+                                height:250
+                            }}> <div className="img3"><img src={dish}/></div> <b>Restaurent Name<br/>
+                            <p className="imgdsc3"><FaClock/><i>15 min</i>  <FaInfoCircle className="info3"/> <FaInfoCircle className="info4"/></p></b><br/>
+                            <p className="home3"><FaHome/><i>$3.45 free delivery</i></p>
+
+
+
+
+
+
+
+
+                            <div className="imgbx4" style={{
+                                border:"1px solid black",
+                                width:150,
+                                height:250
+                            }}> <div className="img4"><img src={dish}/></div> <b>Restaurent Name<br/>
+                            <p className="imgdsc4"><FaClock/><i>15 min</i>  <FaInfoCircle className="info5"/> <FaInfoCircle className="info6"/></p></b><br/>
+                            <p className="home4"><FaHome/><i>$3.45 free delivery</i></p>
+
+                                </div>
+                            </div> 
+
+                        </div>
                     </div>   
                 
                 
@@ -63,12 +147,12 @@ function Hotelmanagment(){
                 {/* myright */}
                  <div style={{
                     border:"1px solid black",
-                    height:"740px",
+                    height:"1500px",
                     width:"340px",
                     borderRadius:"30px",}}>
                         </div>
                         </div>
-                        </div>
+                        </div></div>
 
 
     )
